6aca943c41209837666db371d0ebb2ba69e1bf4f
Shin Toyota <sin.tyt@gmail.com>
2025-03-07 19:23:16 +0900

    初期設定ファイルを追加し、プロジェクトの構成を整えました。また、.gitignoreを更新して生成ファイルを除外し、buf.yamlとbuf.gen.yamlの出力先を修正しました。新しいInstant.protoを追加し、mediapath.protoでのインポートを更新しました。

---
+++ build.rs
@@ -0,0 +1,25 @@
+use std::{fs, path::PathBuf};
+
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let includes = &["protos/v1"];
+    let out_dir = &PathBuf::from("generated/server");
+
+    fs::create_dir_all(out_dir)?;
+
+    let mut config = prost_build::Config::new();
+    config.protoc_arg("--experimental_allow_proto3_optional");
+
+    let protos = &["mediapath.proto"];
+    tonic_build::configure()
+        .out_dir(out_dir)
+        .type_attribute(".", "#[derive(serde::Serialize, serde::Deserialize)]")
+        .type_attribute(".", "#[serde(rename_all = \"camelCase\")]")
+        .build_client(true)
+        .build_server(true)
+        .compile_protos_with_config(config, protos, includes)?;
+
+    let mut config = prost_build::Config::new();
+    config.protoc_arg("--experimental_allow_proto3_optional");
+
+    Ok(())
+}
